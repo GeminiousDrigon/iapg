20d769214e074bd13dc62484e5be08880c7d29f6
vgab18 <vince_gabz14@yahoo.com>
2016-04-18 17:36:12 +0800

    first todo app

---
+++ todo/src/components/TodoApp.js
@@ -0,0 +1,22 @@
+/**
+ * Created by student1 on 4/18/2016.
+ */
+import React from 'react';
+
+export default  class TodoApp extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+
+    render(){
+
+        return (
+           <center> <div>
+            <i><h1><font color="orange">Welcome to ReactJS</font></h1></i>
+        </div>
+               </center>
+    )
+    }
+}

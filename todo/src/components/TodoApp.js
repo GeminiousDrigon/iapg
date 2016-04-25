993e54a7e818b2cb80491b7c130f8e9decdc3740
vgab18 <vince_gabz14@yahoo.com>
2016-04-25 18:04:21 +0800

    new todo-list app

--- todo/src/components/TodoApp.js
+++ todo/src/components/TodoApp.js
@@ -8,31 +8,65 @@ export default  class TodoApp extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            name:'Student'
-        }
+            mytodo:'',
+            'list':[
+                'Vote Duterte',
+                'Kill Corruption'
+            ]
+        };
         //this.onChange = this.onChange.bind(this);
                       }
+        onKeyUp(e){
+            if(e.key==="Enter")
+            {
+                let tmp = this.state.list;
+                tmp.push(this.state.mytodo);
+                this.setState({
+                    list:tmp,
+                    mytodo:''
+                });
+            }
+        }
         onChange(e){
             this.setState({
-                name:e.target.value
+                mytodo:e.target.value
             })
-        }
-
-
+        };
+        onAddItem(){
+                var listx = this.state.list;
+                listx.push(this.state.mytodo);
+                
+                this.setState({
+                    list:listx
+                })
+                }
+            
+        
     render(){
 
+        let items = [];
+        {
+        for(var i=0;i<this.state.list.length;i++)
+        {
+            items.push(<li>{this.state.list[i]}</li>);
+        }
+        }
+        
         return (
-            <center>
+        
            <div>
-            <i><h1><font color="orange">Welcome to ReactJS</font></h1></i>
-            <strong>Enter your name</strong><br/>
-            <form>
-            <input type="text" onChange={this.onChange.bind(this)}/>
-            </form>
-            <br/>
-            <strong>My name is: {this.state.name}</strong>
+            <i><h1><font color="orange">My todo-list</font></h1></i>
+            <input type="text" 
+            onChange={this.onChange.bind(this)} 
+            onKeyUp={this.onKeyUp.bind(this)}
+            placeholder="What needs to be done?"
+            value={this.state.mytodo}
+            autoFocus={true}/>
+            <ol>
+             {items}
+            </ol>
            </div>
-           </center>
-    )
+        
+                )
     }
 }
